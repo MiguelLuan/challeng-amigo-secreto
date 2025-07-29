0f8489fab23230a999beff7c60cd4194973199c3
Migue_Luan <br.miguelcurseiro1000@gmail.com>
2025-07-29 15:26:41 -0300

    Adicionar funcionalidade de adicionar e listar amigos

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const entrada = document.getElementById("amigo");
+const listaAmigos = document.getElementById("listaAmigos");
+
+const listaNomes = [];
+
+function adicionarAmigo(){
+    const nomeAmigo = entrada.value;
+
+    if(nomeAmigo.trim() === "") return;
+
+    const nomeFormatado = nomeAmigo
+        .split(" ")
+        .map( palavra => palavra[0].toUpperCase() + palavra.slice(1).toLowerCase())
+        .join(" ");
+    
+    if(!listaNomes.includes(nomeFormatado)){
+        listaNomes.push(nomeFormatado);
+    }
+    entrada.value = "";
+    console.log(listaNomes);
+    listarAmigos();
+}
+
+function listarAmigos(){
+    listaAmigos.innerHTML = "";
+    for(let nome of listaNomes){
+        const amigo = document.createElement("li");
+        amigo.innerHTML = nome;
+        listaAmigos.appendChild(amigo);
+    }
+
+}
